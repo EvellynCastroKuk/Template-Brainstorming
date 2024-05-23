6a104bd23f4f3f4fcccd4dd23747539366c1b36c
EvellynCastroKuk <165040089+EvellynCastroKuk@users.noreply.github.com>
2024-05-23 12:05:28 -0300

    .

---
+++ script.js
@@ -0,0 +1,17 @@
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
